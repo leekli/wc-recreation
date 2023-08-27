eec0c08743f4a8273eefe969466c50a94a2ed6df
leekli <li.kirkham@live.com>
2023-08-27 21:00:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# wc - word count
+
+### In this repo, I build my own version of the Unix command line tool: `wc`
+
+I have called it `ccwc` - coding challenge word count.
+
+Part of the Coding challenge: https://codingchallenges.fyi/challenges/challenge-wc
+
+Written in: Python 🐍
---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""
+    - ccwc - Coding Challenge Word Count
+    - Recreation of the wc unix command line tool (https://codingchallenges.fyi/challenges/challenge-wc)
+    
+    main.py
+"""
+
+from os import path
+import argparse
+
+
+def main():
+    # Command line arguments setup
+    parser = argparse.ArgumentParser(
+        description="ccwc - word, line, character, and byte count"
+    )
+    parser.add_argument(
+        "-c",
+        type=str,
+        help="The number of bytes in each input file is written to the standard output.",
+    )
+    args = parser.parse_args()
+
+    # If -c flag given
+    if args.c:
+        count_bytes(args.c)
+
+
+def count_bytes(file_name):
+    """Function counts the total bites of a given file.
+
+    Parameters:
+        file_name (str): Path to a file.
+
+    Returns:
+        str: The total file size in bytes and file name.
+
+    Raises:
+        FileNotFoundError: If file name is invalid.
+        OSError: If any other error occurs.
+
+    """
+    try:
+        file_size = path.getsize(file_name)
+        print(f"{file_size} {path.basename(file_name)}")
+    except FileNotFoundError:
+        print("File not found.")
+    except OSError:
+        print("OS error occurred.")
+
+
+if __name__ == "__main__":
+    main()

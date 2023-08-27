bb25b1218a2eedd825fd0d2337e7b55caa72cc17
leekli <li.kirkham@live.com>
2023-08-27 21:13:18 +0100

    adds -l flag functionality

--- main.py
+++ main.py
@@ -21,15 +21,26 @@ def main():
         type=str,
         help="The number of bytes in each input file is written to the standard output.",
     )
+    parser.add_argument(
+        "-l",
+        type=str,
+        help="The number of lines in each input file is written to the standard output.",
+    )
+
     args = parser.parse_args()
+
+    print(args)
 
     # If -c flag given
     if args.c:
-        count_bytes(args.c)
+        print(count_bytes(args.c))
+    # If -l flag given
+    if args.l:
+        print(count_lines(args.l))
 
 
 def count_bytes(file_name):
-    """Function counts the total bites of a given file.
+    """Function counts the total bytes of a given file.
 
     Parameters:
         file_name (str): Path to a file.
@@ -44,11 +55,39 @@ def count_bytes(file_name):
     """
     try:
         file_size = path.getsize(file_name)
-        print(f"{file_size} {path.basename(file_name)}")
+        return f"{file_size} {path.basename(file_name)}"
     except FileNotFoundError:
-        print("File not found.")
+        return "File not found."
     except OSError:
-        print("OS error occurred.")
+        return "OS error occurred."
+
+
+def count_lines(file_name):
+    """Function counts the total lines of a given file.
+
+    Parameters:
+        file_name (str): Path to a file.
+
+    Returns:
+        str: The total number of lines in a file and file name.
+
+    Raises:
+        FileNotFoundError: If file name is invalid.
+        OSError: If any other error occurs.
+
+    """
+    total_lines = 0
+
+    try:
+        with open(file_name, "r") as file:
+            for line in file:
+                total_lines += 1
+
+        return f"{total_lines} {path.basename(file_name)}"
+    except FileNotFoundError:
+        return "File not found."
+    except OSError:
+        return "OS error occurred."
 
 
 if __name__ == "__main__":
